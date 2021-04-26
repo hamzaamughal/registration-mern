c94d6213ee3b36767d63c3cb63d9b67b9c2c15b9
hamzamughal <mughalh473@gmail.com>
2021-04-26 13:09:17 +0500

    mongoDB connection added

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json({ extended: false }))
+
+const dbUrl = 'mongodb+srv://regis:regis123@registration-mern.je3c1.mongodb.net/registration?retryWrites=true&w=majority'
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        })
+        console.log(`MongoDB is Connected..!!`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//connect DataBase
+connectDB()
 
 app.post("/api/register", (req, res) => {
     console.log(req.body);
